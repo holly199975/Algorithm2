65a89440c9e83cd0271fa57d0b770689b1fdb360
Holly Deng <holly199975@gmail.com>
2018-09-26 00:55:20 +0800

    ExchangeSort

---
+++ src/ExchangeSort.java
@@ -0,0 +1,20 @@
+
+public class ExchangeSort {
+
+	public static void main(String[] args) {
+		int n[] = {51,23,6,65,15};
+		for(int i=0; i<n.length - 1; i++){
+			for(int j=(i+1); j<n.length; j++){ 
+				if(n[i] > n[j]){
+					int tmp = n[i]; //tmp代表暫存
+					n[i] = n[j];
+					n[j] = tmp;
+				}
+			}
+			}
+			for(int num: n){ //num是標號的意思
+				System.out.print(num + " ");
+			}
+	}
+
+}
