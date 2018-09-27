ecb713b74da50eebc69b75ba776e5b77545a4679
Holly Deng <holly199975@gmail.com>
2018-09-27 14:21:37 +0800

    Matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -5,10 +5,9 @@ public class Matrix {
 		int A[][] = {{1,2},{3,4},{5,6}}; //acol x arow = 3 x 2
 		int B[][] = {{7,8,9},{5,6,7}};   //bcol x brow = 2 x 3
 		int C[][] = new int[A.length][B[0].length]; // C = acol x brow = 3 x 3
-		
 		for (int acol = 0; acol < A.length; acol++) {
 			   for (int brow = 0; brow < B[0].length; brow++) {
-			    for (int arow = 0; arow < A[0].length; arow++) { //A的第二維
+			    for (int arow = 0; arow < A[0].length; arow++) { //A的第二維(行)
 			     C[acol][brow] = C[acol][brow] + A[acol][arow] * B[arow][brow];
 			    }
 			   }
